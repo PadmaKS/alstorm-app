760935be7ad67a343a071bb320a21c2bfcac1e01
PADMA K S <padks@dhcp-10-120-154-247.wifi.oracle.com>
2024-07-12 09:44:52 +0530

    first commit

---
+++ src/main/java/com/training/bms/AlstormAppApplication.java
@@ -0,0 +1,14 @@
+package com.training.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlstormAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlstormAppApplication.class, args);
+		System.out.println("alstorm app started");
+	}
+
+}
